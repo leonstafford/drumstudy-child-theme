63b679c6abf73600ce7c5c5df09eab4bbcc55b28
Leon Stafford <leon@strattic.com>
2022-03-22 14:38:50 +1100

    test strattic-search.php

---
+++ strattic-search.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Strattic search override template file
+ *
+ * @package DrumChildTheme
+ */
+
+get_header();
+
+?>
+
+<div id="strattic-search-results"></div>
+
+<?php
+
+get_footer();
